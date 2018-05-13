1a786c791e661506a630dc2d60cd5a7390abcb12
汤建锋 <497609288@qq.com>
2018-05-13 22:27:10 +0800

    framWork

---
+++ LongYuBase/LongYuBase/LongYuBase.h
@@ -0,0 +1,19 @@
+//
+//  LongYuBase.h
+//  LongYuBase
+//
+//  Created by 汤建锋 on 2018/5/13.
+//  Copyright © 2018年 AllOverTheSkyStar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LongYuBase.
+FOUNDATION_EXPORT double LongYuBaseVersionNumber;
+
+//! Project version string for LongYuBase.
+FOUNDATION_EXPORT const unsigned char LongYuBaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LongYuBase/PublicHeader.h>
+
+
---
+++ LongYuCore/LongYuCore/LongYuCore.h
@@ -0,0 +1,19 @@
+//
+//  LongYuCore.h
+//  LongYuCore
+//
+//  Created by 汤建锋 on 2018/5/13.
+//  Copyright © 2018年 AllOverTheSkyStar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LongYuCore.
+FOUNDATION_EXPORT double LongYuCoreVersionNumber;
+
+//! Project version string for LongYuCore.
+FOUNDATION_EXPORT const unsigned char LongYuCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LongYuCore/PublicHeader.h>
+
+
